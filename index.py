72454b991b11c7b9d9186c90e17e131b98013368
pmpham <petermphamm@gmail.com>
2021-11-09 20:03:34 -0800

    os fix

--- index.py
+++ index.py
@@ -3,8 +3,8 @@ from discord.ext.commands import Bot
 import botcoms
 
 """ from dotenv import load_dotenv
-load_dotenv()
-import os """
+load_dotenv()"""
+import os
 
 bot = Bot(command_prefix='$')
 TOKEN = '<INSERT TOKEN HERE>'
